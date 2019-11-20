29328338b9529337bc9af671c7d2d4d98149e42b
sethoak <sethoak@gmail.com>
2019-11-20 15:06:22 -0600

    saving files

--- Program.cs
+++ Program.cs
@@ -4,7 +4,28 @@ using System.Linq;
 
 namespace Linq_Challenge
 {
-    // Build a collection of customers who are millionaires
+    /*
+    TASK:
+    As in the previous exercise, you're going to output the millionaires,
+    but you will also display the full name of the bank. You also need
+    to sort the millionaires' names, ascending by their LAST name.
+
+    Example output:
+        Tina Fey at Citibank
+        Joe Landy at Wells Fargo
+        Sarah Ng at First Tennessee
+        Les Paul at Wells Fargo
+        Peg Vale at Bank of America
+*/
+
+    // Define a bank
+    public class Bank
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+    }
+
+    // Define a customer
     public class Customer
     {
         public string Name { get; set; }
@@ -16,33 +37,44 @@ namespace Linq_Challenge
     {
         public static void Main()
         {
-            List<Customer> customers = new List<Customer>()
-            {
-                new Customer() { Name = "Bob Lesman", Balance = 80345.66, Bank = "FTB" },
-                new Customer() { Name = "Joe Landy", Balance = 9284756.21, Bank = "WF" },
-                new Customer() { Name = "Meg Ford", Balance = 487233.01, Bank = "BOA" },
-                new Customer() { Name = "Peg Vale", Balance = 7001449.92, Bank = "BOA" },
-                new Customer() { Name = "Mike Johnson", Balance = 790872.12, Bank = "WF" },
-                new Customer() { Name = "Les Paul", Balance = 8374892.54, Bank = "WF" },
-                new Customer() { Name = "Sid Crosby", Balance = 957436.39, Bank = "FTB" },
-                new Customer() { Name = "Sarah Ng", Balance = 56562389.85, Bank = "FTB" },
-                new Customer() { Name = "Tina Fey", Balance = 1000000.00, Bank = "CITI" },
-                new Customer() { Name = "Sid Brown", Balance = 49582.68, Bank = "CITI" }
-            };
-            var groups = customers.Where(c => c.Balance >= 1_000_000)
-                .GroupBy(c => c.Bank);
+            // Create some banks and store in a List
+            List<Bank> banks = new List<Bank>() {
+            new Bank(){ Name="First Tennessee", Symbol="FTB"},
+            new Bank(){ Name="Wells Fargo", Symbol="WF"},
+            new Bank(){ Name="Bank of America", Symbol="BOA"},
+            new Bank(){ Name="Citibank", Symbol="CITI"},
+        };
 
-            // key name is the bank name
-            foreach (var group in groups)
-            {
-                Console.WriteLine($"{group.Key} has {group.Count()} customers");
+            // Create some customers and store in a List
+            List<Customer> customers = new List<Customer>() {
+            new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+            new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+            new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+            new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+            new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+            new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+            new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+            new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+            new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+            new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+        };
 
-            }
+            /*
+                You will need to use the `Where()`
+                and `Select()` methods to generate
+                instances of the following class.
 
-            foreach (Customer customer in groups)
+                public class ReportItem
+                {
+                    public string CustomerName { get; set; }
+                    public string BankName { get; set; }
+                }
+            */
+            List<ReportItem> millionaireReport = ...
+
+        foreach (var item in millionaireReport)
             {
-                Console.WriteLine($"****{customer.Name}");
+                Console.WriteLine($"{item.CustomerName} at {item.BankName}");
             }
         }
     }
-}
