0b6e49f91b445718bc8352fa5c329fe95bdd3ebb
sethoak <sethoak@gmail.com>
2019-11-20 14:52:38 -0600

    saving code

--- Program.cs
+++ Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Linq_Challenge
 {
-    class Program
+    // Build a collection of customers who are millionaires
+    public class Customer
     {
-        static void Main(string[] args)
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public string Bank { get; set; }
+    }
+
+    public class Program
+    {
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            List<Customer> customers = new List<Customer>()
+            {
+                new Customer() { Name = "Bob Lesman", Balance = 80345.66, Bank = "FTB" },
+                new Customer() { Name = "Joe Landy", Balance = 9284756.21, Bank = "WF" },
+                new Customer() { Name = "Meg Ford", Balance = 487233.01, Bank = "BOA" },
+                new Customer() { Name = "Peg Vale", Balance = 7001449.92, Bank = "BOA" },
+                new Customer() { Name = "Mike Johnson", Balance = 790872.12, Bank = "WF" },
+                new Customer() { Name = "Les Paul", Balance = 8374892.54, Bank = "WF" },
+                new Customer() { Name = "Sid Crosby", Balance = 957436.39, Bank = "FTB" },
+                new Customer() { Name = "Sarah Ng", Balance = 56562389.85, Bank = "FTB" },
+                new Customer() { Name = "Tina Fey", Balance = 1000000.00, Bank = "CITI" },
+                new Customer() { Name = "Sid Brown", Balance = 49582.68, Bank = "CITI" }
+            };
+            var groups = customers.Where(c => c.Balance >= 1_000_000)
+                .GroupBy(c => c.Bank);
+
+            // key name is the bank name
+            foreach (var group in groups)
+            {
+                Console.WriteLine($"{group.Key} has {group.Count()} customers");
+
+            }
+
+            foreach (Customer customer in groups)
+            {
+                Console.WriteLine($"****{customer.Name}");
+            }
         }
     }
 }
